04c39d4b03e36e85be4377349b240db2aa6d4809
Darius I. Karel <me@dikarel.com>
2017-01-22 14:17:32 -0800

    code cleanup

--- test/components/Widget.spec.js
+++ test/components/Widget.spec.js
@@ -12,8 +12,8 @@ describe('Widget', () => {
   describe('when initially loading', () => {
     it('renders a PlaceholderRow component for each profile', () => {
       const profiles = List.of(null, null, null)
-      const wrapper = shallow(<Widget profiles={profiles} />)
-      expect(wrapper.find(PlaceholderRow).length).toBe(3)
+      const widget = shallow(<Widget profiles={profiles} />)
+      expect(widget.find(PlaceholderRow).length).toBe(3)
     })
   })
 
@@ -21,70 +21,70 @@ describe('Widget', () => {
     it('renders a ProfileRow component for each profile', () => {
       const profiles = List.of({}, {}, {})
       const profileStates = List.of(null, null, null)
-      const wrapper = shallow(<Widget profiles={profiles} profileStates={profileStates} />)
-      expect(wrapper.find(ProfileRow).length).toBe(3)
+      const widget = shallow(<Widget profiles={profiles} profileStates={profileStates} />)
+      expect(widget.find(ProfileRow).length).toBe(3)
     })
   })
 
   it('renders a refresh button', () => {
       const profiles = List()
       const profileStates = List()
-      const wrapper = shallow(<Widget profiles={profiles} profileStates={profileStates} />)
-      expect(wrapper.text()).toMatch(/refresh/i)
+      const widget = shallow(<Widget profiles={profiles} profileStates={profileStates} />)
+      expect(widget.text()).toMatch(/refresh/i)
   })
 
   it('renders a view all button', () => {
       const profiles = List()
       const profileStates = List()
-      const wrapper = shallow(<Widget profiles={profiles} profileStates={profileStates} />)
-      expect(wrapper.text()).toMatch(/view all/i)
+      const widget = shallow(<Widget profiles={profiles} profileStates={profileStates} />)
+      expect(widget.text()).toMatch(/view all/i)
   })
 
   describe('when it is throttled by GitHub', () => {
-    let wrapper = null
+    let widget = null
 
     beforeEach(() => {
-      wrapper = shallow(<Widget profiles={List()} profileStates={List()} state={GithubThrottledState}/>)
+      widget = shallow(<Widget profiles={List()} profileStates={List()} state={GithubThrottledState}/>)
     })
 
     it('renders an error message', () => {
-      expect(wrapper.text()).toMatch(/throttled by github/i)
+      expect(widget.text()).toMatch(/throttled by github/i)
     })
 
     it('offers a link to an explanation', () => {
-      expect(wrapper.text()).toMatch(/find out more/i)
+      expect(widget.text()).toMatch(/find out more/i)
     })
   })
 
   describe('when it is possibly offline', () => {
-    let wrapper = null
+    let widget = null
 
     beforeEach(() => {
-      wrapper = shallow(<Widget profiles={List()} profileStates={List()} state={PossiblyOfflineState}/>)
+      widget = shallow(<Widget profiles={List()} profileStates={List()} state={PossiblyOfflineState}/>)
     })
 
     it('renders an error message', () => {
-      expect(wrapper.text()).toMatch(/offline/i)
+      expect(widget.text()).toMatch(/offline/i)
     })
 
     it('offers an option to try again', () => {
-      expect(wrapper.text()).toMatch(/try again/i)
+      expect(widget.text()).toMatch(/try again/i)
     })
   })
 
   describe('when there is a generic load error', () => {
-    let wrapper = null
+    let widget = null
 
     beforeEach(() => {
-      wrapper = shallow(<Widget profiles={List()} profileStates={List()} state={ErrorState}/>)
+      widget = shallow(<Widget profiles={List()} profileStates={List()} state={ErrorState}/>)
     })
 
     it('renders an error message', () => {
-      expect(wrapper.text()).toMatch(/failed/i)
+      expect(widget.text()).toMatch(/failed/i)
     })
 
     it('offers an option to try again', () => {
-      expect(wrapper.text()).toMatch(/try again/i)
+      expect(widget.text()).toMatch(/try again/i)
     })
   })
 })
