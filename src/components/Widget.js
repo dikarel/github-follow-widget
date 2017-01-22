3a356c77847e7b883af59a5bcb81c245cba3bb2e
Darius I. Karel <me@dikarel.com>
2017-01-22 00:14:37 -0800

    code cleanup

--- src/components/Widget.js
+++ src/components/Widget.js
@@ -5,25 +5,34 @@ import React from 'react'
 
 export default function Widget (props) {
   const {profiles, states, onReloadAll, onReload} = props
-  const rows = (profiles || new List())
-    .toJS()
-    .map((profile, i) => {
-      if (!profile) return <PlaceholderRow key={i} />
-      return <ProfileRow profile={profile} state={states.get(i)} onReload={() => onReload(i)} key={i} />
-    })
 
   return (
     <div className='suggestions-panel panel panel-default'>
       <div className='panel-body'>
-        <h4 className='suggestions-heading'>
-          Who to follow
-          <span className='suggestions-refresh'>
-            &nbsp;&middot;&nbsp;
-            <a onClick={onReloadAll}>Refresh</a>
-          </span>
-        </h4>
-        {rows}
+        {header()}
+        {rows()}
       </div>
     </div>
   )
+
+  function header () {
+    return (
+      <h4 className='suggestions-heading'>
+        Who to follow
+        <span className='suggestions-refresh'>
+          &nbsp;&middot;&nbsp;
+          <a onClick={onReloadAll}>Refresh</a>
+        </span>
+      </h4>
+    )
+  }
+
+  function rows () {
+    return (profiles || new List())
+      .toJS()
+      .map((profile, i) => {
+        if (!profile) return <PlaceholderRow key={i} />
+        return <ProfileRow profile={profile} state={states.get(i)} onReload={() => onReload(i)} key={i} />
+      })
+  }
 }
