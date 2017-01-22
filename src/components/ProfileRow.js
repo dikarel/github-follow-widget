9d824206d75c115b9d210f0525b8eba99923bcaa
Darius I. Karel <me@dikarel.com>
2017-01-22 11:03:54 -0800

    open profiles in new tab

--- src/components/ProfileRow.js
+++ src/components/ProfileRow.js
@@ -20,7 +20,7 @@ export default function ProfileRow (props) {
           &nbsp;
           <span className='suggestions-username'>{username}</span>
         </h5>
-        <a className='btn btn-sm btn-default' href={profileUrl} role='button'>
+        <a className='btn btn-sm btn-default' href={profileUrl} role='button' target='_blank'>
           <span className='glyphicon glyphicon-user' />
           &nbsp;
           Profile
